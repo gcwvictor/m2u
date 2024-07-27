dba44b63cc81750c37e0cad135f60d24cc68d07d
figoperdana <perdanaputrafigo@gmail.com>
2024-07-27 22:39:48 +0700

    add code v38

--- static/js/temuan_gangguan.js
+++ static/js/temuan_gangguan.js
@@ -103,6 +103,43 @@ async function handleSubmit(event) {
     }
 }
 
+function resizeImage(file, maxWidth, maxHeight, callback) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = function(event) {
+        const img = new Image();
+        img.src = event.target.result;
+
+        img.onload = function() {
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+
+            let width = img.width;
+            let height = img.height;
+
+            if (width > height) {
+                if (width > maxWidth) {
+                    height *= maxWidth / width;
+                    width = maxWidth;
+                }
+            } else {
+                if (height > maxHeight) {
+                    width *= maxHeight / height;
+                    height = maxHeight;
+                }
+            }
+
+            canvas.width = width;
+            canvas.height = height;
+            ctx.drawImage(img, 0, 0, width, height);
+
+            const dataUrl = canvas.toDataURL('image/jpeg');
+            callback(dataUrl);
+        }
+    }
+}
+
 async function displayTableData() {
     try {
         const response = await fetch('/getGangguanData');
