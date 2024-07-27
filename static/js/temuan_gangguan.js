8f7b89e40251833fc360d14320002d22fcb09efa
figoperdana <perdanaputrafigo@gmail.com>
2024-07-27 20:25:32 +0700

    add code v26

--- static/js/temuan_gangguan.js
+++ static/js/temuan_gangguan.js
@@ -50,10 +50,8 @@ async function handleSubmit(event) {
 
     const file = formData.get('foto');
     if (file) {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onloadend = async function () {
-            data.foto = reader.result;
+        resizeImage(file, 800, 600, async (base64Image) => {
+            data.foto = base64Image;
 
             try {
                 const response = await fetch('/saveGangguanData', {
@@ -76,10 +74,46 @@ async function handleSubmit(event) {
             }
 
             event.target.reset();
-        };
+        });
     }
 }
 
+function resizeImage(file, maxWidth, maxHeight, callback) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = function(event) {
+        const img = new Image();
+        img.src = event.target.result;
+
+        img.onload = function() {
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+
+            let width = img.width;
+            let height = img.height;
+
+            if (width > height) {
+                if (width > maxWidth) {
+                    height *= maxWidth / width;
+                    width = maxWidth;
+                }
+            } else {
+                if (height > maxHeight) {
+                    width *= maxHeight / height;
+                    height = maxHeight;
+                }
+            }
+
+            canvas.width = width;
+            canvas.height = height;
+            ctx.drawImage(img, 0, 0, width, height);
+
+            const dataUrl = canvas.toDataURL('image/jpeg');
+            callback(dataUrl);
+        }
+    }
+}
 
 async function displayTableData() {
     try {
@@ -155,7 +189,7 @@ async function deleteRow(id) {
     }
 }
 
-document.getElementById('exportTable').addEventListener('click', function () {
+document.getElementById('exportTable').addEventListener('click', function() {
     exportTableData();
 });
 
