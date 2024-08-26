0a30bb4e5b3dba234d63bbdcd66dbd38e19da238
Victor Gemilang <90447370+gcwvictor@users.noreply.github.com>
2024-08-26 05:01:10 +0000

    remove entry

--- static/js/jkm_harian.js
+++ static/js/jkm_harian.js
@@ -239,7 +239,7 @@ async function exportTableToExcel() {
             filteredData.forEach(entry => {
                 rows.push([
                     entry.tanggal,
-                    entry.mesinNamesMap[unit_mesin],  // Gunakan nama mesin yang dikustomisasi
+                    mesinNamesMap[unit_mesin],  // Gunakan nama mesin yang dikustomisasi
                     entry.jkm_harian,
                     entry.jumlah_jkm_har,
                     entry.jsmo,
